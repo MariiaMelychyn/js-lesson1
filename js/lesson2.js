ae5170b631a4ddb8df0e2d69377250eeaa74e2a3
Mariia <79396737+MariiaMelychyn@users.noreply.github.com>
2023-01-22 00:56:24 +0200

    Update lesson2.js

--- js/lesson2.js
+++ js/lesson2.js
@@ -1,165 +1,29 @@
-
-
-// ДЛЯ ТОГО , ЩОБ КОНСОЛЬ ЛОГ СПРАЦЬОВУВАВ ПОТРІБНО ВСТАНОВИТИ ПЛАГІН JSSNIPPETS
-
-
-
-// 1) базовые операции с массивом
-// Создайте массив styles с элементами «Джаз» и «Блюз».
-// Добавьте «Рок-н-ролл» в конец.
-// Замените значение в середине на «Классика».
-// Удалите первый элемент и выведите его в консоль.
-// Вставьте «Рэп» и «Регги» в начало массива.
-
-// Массив по ходу выполнения операций:
-// Джаз, Блюз
-// Джаз, Блюз, Рок-н-ролл
-// Джаз, Классика, Рок-н-ролл
-// Классика, Рок-н-ролл
-// Рэп, Регги, Классика, Рок-н-ролл
-
-// const styles = ["Джаз", "Блюз"];
-// styles.push("Рок-н-ролл");//добавили
-// console.log(styles)
-
-// styles.splice(1, 1, "Классика");//поміняли, або варіант //splice - може добавити декілька елементів
-// styles[1] = "Классика"
-
-// const deletedStyle = styles.shift(); //видаляємо елемент перший, напрклад для того , що ми можемо елемент видалити із початку масива і поставити у кінець масива
-// styles.unshift("Рэп", "Регги"); //вставляємо у початок масиву
-// console.log(deletedStyle);
-// console.log(styles);
-
-
-
-
 // ЩО КОНСОЛЬ ВИВЕДЕ НА ЕКРАН
 // const roles = ["user", "tester", "admin"];
-// console.log(roles[1]); //tester
-// console.log(roles[0]); //user
-// console.log(roles[5]); //undefined, тому що у нам такого не має
-// console.log(roles.length);//3
-// console.log(roles[-1]);//undefined , тому що мінусового індексу у js не існує
-// const lastIndex = roles.length - 1;//admin, тому що у нас є рядок, а length -1 , ми отримуємо доступ до останнього елемента масиву 
-// console.log(roles[lastIndex]);//admin
+// console.log(roles[1]); 
+// console.log(roles[0]); 
+// console.log(roles[5]); 
+// console.log(roles.length);
+// console.log(roles[-1]);
+// const lastIndex = roles.length - 1;
+// console.log(roles[lastIndex]);
 
 // roles.pop();
-// console.log(roles);// ['user', 'tester']
+// console.log(roles);
 
-// const result = roles.push("support");//
-// console.log(roles); // ['user', 'tester', 'admin', 'support']
+// const result = roles.push("support");
+// console.log(roles); 
 
 // roles.shift();
-// console.log(roles);//['tester', 'admin']
+// console.log(roles);
 
 // roles.unshift("manager");
-// console.log(roles);// ['manager', 'user', 'tester', 'admin']
+// console.log(roles);
 
 // roles.splice(2, 3);
-// console.log(roles);// ['user', 'tester']
-
-// roles.splice(2, 1, "user2");
-// roles.splice(2, 0, "user2");
-// console.log(roles);//['user', 'tester', 'user2', 'user2']
-
-// const newArr = roles.slice();
-// console.log("newArr", newArr);// ['user', 'tester', 'admin']
-// console.log(roles === newArr);//false
-// console.log(roles.includes("tester"));//true include повертає одне із 2-х варіантів тру або фолс
+// console.log(roles);
 
 
-
-// 2) Напиши функцию logItems(array), которая получает массив и использует цикл for, который для каждого элемента массива
-//будет выводить в консоль сообщение в формате < номер элемента > - <значение элемента >.
-//Нумерация элементов должна начинаться с 1.
-// Например для первого элемента массива['Mango', 'Poly', 'Ajax'] с индексом 0 будет выведено
-// 1 - Mango, а для индекса 2 выведет 3 - Ajax.
-
-const users = ["Mango", "Poly", "Ajax"]; // 3
-
-// for (let i = 0; i < users.length; i++) {
-//   console.log("index", i); // 0 // 1 // 2
-//   console.log("value", users[i]); // users[0] // users[1] // users[2]
-// }
-
-// for (let user of users) {
-//   console.log("index", users.indexOf(user));
-//   console.log(user); // Mango //Poly  // Ajax
-// }
-
-// const logItems = function (array){
-// for (let i = 0; i < users.length; i++) {
-//   console.log("index", i); // 0 // 1 // 2
-//   console.log("value", users[i]); // users[0] // users[1] // users[2]
-// }
-// }
-// logItems();
-
-
-
-// 3) Напиши код который ищет самое маленькое число в массиве.
-
-// const numbers = [2, 17, 94, 1, 23, 37];
-
-// 1) Определить стартовое минимальное число
-// 2) Сравнить это число со всеми последующими елементами масива
-// 3) Если стартовое число меньше числа с которым сравниваем - оставляем как есть
-// 4) Если стартовое число больше числа с которым срасниваем - пререзаписываем минимальное число
-
-// let smallestNumber = numbers[0]; // 2 тут ми получаємо стартове число
-
-// for (let number of numbers) { // при ппершому проході циклу ми получаємо 2 потім порівнюємо 2 > 17 фолс, 94 більше 2 пропускаємо, і так поки початкове число не знайде меншого тоді виводить
-//   // 2 // 17 // 94 // 1 // 23 // 37
-//   if (smallestNumber > number) {
-//     smallestNumber = number; // 1
-//   }
-// }
-
-// console.log(smallestNumber); // 1
-
-// const findSmallestNumber = function (numbers) {
-//   let smallestNumber = numbers[0];
-
-//   for (const number of numbers) {
-//     if (smallestNumber > number) {
-//       smallestNumber = number;
-//     }
-//   }
-
-//   return smallestNumber;
-// };
-
-// console.log(findSmallestNumber([2, 17, 94, 1, 23, 37])); // 1
-// console.log(findSmallestNumber([49, 4, 83, 7, 12])); // 4
-
-
-
-// 4) Проверить является ли строка палиндромом
-//таку задачу дають на співбесідаї
-//палиндромом - це число яке читає з права на ліво та з ліва на право одинаково, напрклад довод
-//
-
-// const str = "ротатор"; // тут має получитись тру 
-// const newString = "Hello"; //тут фолс
-
-// 1) Сделать со строки масив
-// 2) Перевевнуть масив наоборот
-// 3) С масива сделать строку
-// 4) Сравнить 2 строки
-
-// const array = str.split("");// розбиваємо по символьно, і получимо масив з буквами
-// array.reverse(); //робить рядок наоборот
-// const reverseString = array.join(""); //получаємо новий рядок але наоборот
-// console.log(reverseString);
-// console.log(str === reverseString)
-
-//КОД ПО ЦЕПОЧКИ
-// const reverseStringWithChaining = newString.split("").reverse().join("");
-// console.log(newString === reverseStringWithChaining);
-
-
-
-//2 МОДУЛЬ 2 ЗАНЯТТЯ
 //ДАВАЙТЕ ПЕРЕВІРЕМО ОБЛАСТЬ ВИДИМОСТІ Ф-ЦІЇ
 
 // 1)
@@ -172,7 +36,7 @@ const users = ["Mango", "Poly", "Ajax"]; // 3
 
 //   return a + b;
 // }
-// console.log(sum()); //150- БО МИ ОТРИМУЄМО ДАНІ У СЕРЕДИНІ Ф-ЦІЇ
+// console.log(sum()); 
 
 
 // 2)
@@ -182,7 +46,7 @@ const users = ["Mango", "Poly", "Ajax"]; // 3
 // function sum() {
 //   return a + b;
 // }
-// console.log(sum())//15 - ТОМУ ЩО , У СЕРЕДИНІ Ф-ЦІЇ НЕМАЄ ПЕРЕМІННИХ, ЧИ БЕРЕМО ЇХ З ГЛОБАЛЬНОЇ ОБЛАСТІ ВИДИМОСТІ
+// console.log(sum())
 
 
 // 3)
@@ -192,7 +56,7 @@ const users = ["Mango", "Poly", "Ajax"]; // 3
 // function sum(a, b) {
 //   return a + b;
 // }
-// console.log(sum(100, 300))//400, ТОМУ ЩО ТУТ РОБОТА З НАШИМИ АРГУМЕНТАМИ, ТОДІ ІГНОРУЮТЬСЯ ГЛОБАЛЬНІ ПЕРЕМІННІ
+// console.log(sum(100, 300))
 
 
 // 4)
@@ -209,15 +73,14 @@ const users = ["Mango", "Poly", "Ajax"]; // 3
 
 //   return a + b;
 // }
-// console.log(sum())//20, У НАС 2 Ф-ЦІЇ sum2 МИ НЕ ЗАПУСТИЛИ, ТОМУ ВОНА ІГНОРУЄТЬСЯ, ЦН ТО САМЕ, ЩО Б ЇЇ НЕ БУЛО
-
+// console.log(sum())
 
 // 5)
 // let a = 5;
 // let b = 10;
 
 // function sum() {
-//   let a = sum2(); // 40
+//   let a = sum2(); 
 
 //   function sum2() {
 //     let a = 30;
@@ -227,283 +90,19 @@ const users = ["Mango", "Poly", "Ajax"]; // 3
 //   return a + b;
 // }
 
-// console.log(sum()); // 50 ТОМУ ЩО  let a = sum2(); ВИКОНУЄ Ф-ЦІЮ РЕТИРН, І ТОДІ ДОДАЄ Б
+// console.log(sum());
 
 
 
-// ЗАДАЧІ
 
-//1) Напиши функцию calculateAverage() которая принимает произвольное кол-во аргументов и возвращает их
-// среднее значение.Все аругменты будут только числами.
-
-// 1) Нужно посчитать суму всех аргументов
-//2) Посчитать средьнее значение
-//3) Вывести результат на экран
-
-// function calculateAverage() {
-//   console.log("arguments", arguments);//ARGUMENTS,НАША ЗАДАЧА ВЗЯТИ МАСИВ НАШ ЩОБ ПРАЦЮВАТИ
-//   const numbers = Array.from(arguments);//ЯК НАМ ЗРОБИТИ З ПСЕВДОМАСИВА МАВИС, БО АРГУМЕНТ ЦЕ ПСЕВДОМАСИВ
-// ARRAY - ЦЕ КОНСТРУКЦІЯ , ЯКА МОЖЕ СТВОРИТИ МАСИВИ, FROM - ЦЕ ДОДАТКОВИЙ МЕТОД , ЯКИЙ МОЖЕ ЗРОБИТИ З ДОДАТКОВОЇ КОЛЕКЦІЇ МАСИВ
-
-//   let total = 0;
-//   for (let number of numbers) {
-//     total += number;
-//   }
-//   const length = numbers.length;
-//   const average = total / length;
-//   return average;
+// КАХУТ
+// const checkString = (str) => {
+//     let exp;
+//     if (str || 0){
+//         exp = false
+//     } else {
+//         exp = true
+//     }
+//     console.log(exp)
 // }
-//
-
-//ТЕПЕР СКОРОТИМО КОД
-// function calculateAverage() {//ФАНШИН ДЕКЛАРЕЙШИН
-//   console.log("arguments", arguments);
-//   const numbers = Array.from(arguments);
-//   let total = 0;
-//   for (let number of numbers) {
-//     total += number;
-//   }
-//   return (total / numbers.length).toFixed(2);
-// }
-
-// console.log(calculateAverage(1, 2, 3, 4)); // 2.5
-// console.log(calculateAverage(14, 8, 2)); // 8
-// console.log(calculateAverage(27, 43, 2, 8, 36)); // 23.2
-
-
-// 2) Напишите функцию greet(name), которая при вызове будет получать имя(например, «Василий») и логировать строку «Привет, <имя>».
-// В случае отсутствующего аргумента выводить «Привет, гость»
-
-// const greet = function (name = "гость") {
-//   return `Привет ${name}`;
-// };
-
-// console.log(greet("Манго"));
-// console.log(greet());
-
-// const greet = function (name = "гость") {//ФАНШИН ЕКСПРЕШИН
-//   console.log(`Привет ${name}`);
-// };
-
-// greet("Манго");
-// greet();
-
-// name = "гость" -ЦЕ ДЕФОЛТНИЙ ПАРАМЕНТР, ПО ДЕФОЛТУ МОЖНА ПОСТАВИТИ ЗНАЧАННЯ, ЯКЩО ЗНАЧЕННЯ НЕМАЄ
-
-
-
-// 3) Выполните рефакторинг заменив объявление функции на стрелочную функцию.
-
-// function checkNumbers(a, b) { //ФАНШИН ДЕКЛАРЕЙШИН
-//   if (a > b) {
-//     return `число ${a} больше ${b}`;
-//   }
-
-//   return `число ${b} больше ${a}`;
-// }
-
-// const checkNumbers = function (a, b) {//ФАНШИН ЕКСПРЕШИН
-//   if (a > b) {
-//     return `число ${a} больше ${b}`;
-//   }
-
-//   return `число ${b} больше ${a}`;
-// };
-
-// const checkNumbers = (a, b) => {//СТРІЛОЧНА Ф-ЦІЯ
-//   if (a > b) {
-//     return `число ${a} больше ${b}`;
-//   }
-
-//   return `число ${b} больше ${a}`;
-// };
-
-// const checkNumbers = (a, b) => {//ПЕРЕВОДИМО У ТЕРНАРНИЙ ОПЕРАТОР
-//   return a > b ? `число ${a} больше ${b}` : `число ${b} больше ${a}`;
-// };
-
-// const checkNumbers = (a, b) => //ПРИ ЗАПИСІ СТРІЛОЧНОЇ Ф-ЦІЇ МИ МОЖЕМО ВІДКАЗАТИСЯ ВІД ФІГУРНИХ ДУЖОК І ВІД РЕТУРН, ЦЕ НАЗИВАЄТЬСЯ НЕ ЯВНИЙ РЕТОРН
-//   a > b ? `число ${a} больше ${b}` : `число ${b} больше ${a}`;
-
-
-
-// 4) Написать ф - ю capitalize которая буде принимать строку и будет возвращать новую где каждое слово будет с большой буквы
-// capitalize('the quick brown fox') // 'The Quick Brown Fox '
-
-// function capitalize(string) {
-//   const arrayFromString = string.split(" ");//1)РОЗБИТИ РЯДОК НА СЛОВА
-//   const newArray = [];//4) СТВ МОСИВ ДЕ БУДЕМО ЗАПУШУВАТИ НОВИЙ МАСИВ
-
-//   for (let word of arrayFromString) { 2)СТВОРЮЄМО ЦИКЛ
-//     let firstLetter = word[0].toUpperCase();//3) ПЕРЕПИСУЄМО КОЖНУ ПЕРШУ БУКВУ У СЛОВІ, І ПЕРЕВОДИМО У ВЕЛИКУ БУКВУ
-    // let restString = word.slice(1);//SPLISE- МНТОДИ МАСИВУ , А SLICE ДЛЯ РЯДКА , ТЕПЕР МИ СТВ НОВУ ЗМІННУ , У ЯКІ МИ ВИРІЗАЄМО ДО 1 ІНДЕКСА РЯДОК
-    // let finalString = `${firstLetter}${restString}`;//ТЕПЕР ОБЄДНУЄМО 
-    // newArray.push(finalString);//5) ПУШИМО ДО МАСИВУ 
-//   }
-//   return newArray.join(" ");// 6)ПОЛУЧАЄМО НОВИЙ РЯДОК З ПРОБІЛОМ 
-// }
-
-// console.log(capitalize("the quick brown fox"));
-
-
-
-// 5) Напишите функции для работы с коллекцией обучающих курсов courses:
-
-// addCourse(name) - добавляет курс в конец коллекции
-// removeCourse(name) - удаляет курс из коллекции
-// updateCourse(oldName, newName)- изменяет имя на новое
-
-// const courses = ["HTML", "CSS", "JavaScript", "React", "PostgreSQL"];
-
-// function addCourse(course) {
-//   if (courses.includes(course)) {//2) ТУТ МИ ПРОВІРЯЄМО ЧИ Є У НАС КУРС, ПИШЕМО ПРОВІРКУ
-//     console.log("У вас уже есть такое курс");
-//     return;
-//   }
-//   courses.push(course);1) ТУ МИ ПУШИМО КУРС 
-// }
-
-// const addCourse = (course) => {
-//   if (courses.includes(course)) {
-//     console.log("У вас уже есть такое курс");
-//     return;
-//   }
-//   courses.push(course);
-// };
-
-// const removeCourse = (course) => {//СПОЧАТКУ НАМ ПОТРІБНО ЗНАЙТИ ІНДЕКС КУРСА, А ПОТІМ ЙОГО ПОТРІБНО ВИДАЛИТИ ЧЕРЕЗ SPLICE
-//   const courseIndex = courses.indexOf(course);//1) ПОЛУЧАЄМО ІНДЕКС
-//   if (courseIndex === -1) {//3) ПИШЕМО ПРОВІРКУ , НА ІФ, ЯКЩО НАШ ІНДЕКСТ НЕ РІВНЯЄТЬСЯ -1 ВИДАЄМО ПОВІДОМЛЕННЯ
-//     console.log("Курс с таким имененем не найден");
-//     return;
-//   }
-//   courses.splice(courseIndex, 1);//2) ВИДАЛЯЄМО
-// };
-
-// const updateCourse = (courseToDelete, courseToAdd) => {
-//   const index = courses.indexOf(courseToDelete);//1) ЗНАХОДИМО ІНДЕКС 
-//   courses.splice(index, 1, courseToAdd)// ПОТРІЮНО ЗАБРАТИ , УДАЛИТИ 1 ЕЛЕМЕНТ ТА ЗАМІНИТИ
-    // courses[index] = courseToAdd;//ЦЕ ВЖЕ 2 ВАРІАНТ
-//   //   removeCourse(courseToDelete);//ЦЕ ВЖЕ 3 ВАРІАНТ
-//   //   addCourse(courseToAdd);//ЦЕ ВЖЕ 3 ВАРІАНТ
-// };
-
-// addCourse("Express"); // ['HTML', 'CSS', 'JavaScript', 'React', 'PostgreSQL', 'Express']
-// addCourse("CSS"); // 'У вас уже есть такое курс'
-// removeCourse("React"); // ['HTML', 'CSS', 'JavaScript', 'PostgreSQL', 'Express']
-// removeCourse("Vue"); // 'Курс с таким имененем не найден'
-// updateCourse("Express", "NestJS"); // ['HTML', 'CSS', 'JavaScript', 'PostgreSQL', 'NestJS']
-
-// console.log(courses);
-
-
-
-
-
-
-
-
-
-// МОДУЛЬ 3 1 ЗАНЯТТЯ
-
-
-
-// 1 ЗАДАЧА)  Напиши скрипт, который, для объекта user, последовательно:
-
-// добавляет поле mood со значением 'happy'
-// заменяет значение hobby на 'skydiving'
-// заменяет значение premium на false
-// выводит содержимое объекта user в формате ключ:значение используя Object.keys() и for...of
-
-// const user = {
-//   name: "Mango",
-//   age: 20,
-//   hobby: "html",
-//   premium: true,
-// };
-
-//1) PYSH НЕ ВИКОРИСТОВУЄМ, БО ЦЕ МЕТОД МАСИВУ 
-// user.mood = "happy";
-// user.hobby = "skydiving";
-// user.premium = false;
-
-// 2)ОТРИМУЄМО ВСІ КЛЮЧІ
-// const keys = Object.keys(user);
-// console.log(`keys`, keys);
-
-// 3) ЗА ДОПОМОГОЮ FOR OF ОТРИМУЄМО КЛЮЧ ЗНАЧЕННЯ
-// for (let key of keys) {//ТУТ МИ ПОЛУЧАЄМО МАСИВ КЛЮЧЕЙ
-//   // key = 'name'
-//   // key = 'age'
-//   console.log(`${key} : ${user[key]}`); //[KEY] ЦЕ ДИНАМІЧНЕ ЗНАЧЕННЯ , МИ НЕ ЗНАЄМО ЯКЕ ЗНАЧЕННЯ ЗАПИШЕТЬСЯ,ТОМУ МИ ЗАПИСУЄМО У КВАДРАТНІ ДУЖКИ
-// }
-
-// console.log(user);
-
-// 2 ЗАДАЧА) У нас есть объект, в котором хранятся зарплаты нашей команды.
-// Напишите код для суммирования всех зарплат и сохраните результат в переменной sum.
-// Должно получиться 390. Если объект salaries пуст, то результат должен быть 0.
-
-// const salaries = {
-//   John: 100,
-//   Ann: 160,
-//   Pete: 130,
-// };
-//ЦЕ МОЖНА ВИКОРИСТОВУВАТИ ДЕКІЛЬКОМА СПОСОБАСИ, FOR IN, А МОЖНА СТАНДАРТНИЙ FOR OF
-
-// const money = Object.values(salaries);//1) ОТРИМУЄМО ЗНАЧЕННЯ І ЗАПИСУЄМО У МАСИВ
-// console.log(`money`, money);
-// let total = 0; //3) СТВ ЗМІННУ ДО ЯКОЇ МИ БУДЕМО ДОДАВАТИ
-
-// for (let element of money) {2) ТЕПЕР НАМ ПОТРІБНО ДОДАТИ
-//   total += element;
-// }
-
-// console.log(`total`, total);
-
-
-//2-Й ВАРІАНТ, ЩОБ МОЖНА БУЛО ПЕРЕВИКОРИСТОВУВАТИ ЛОГІКУ, ТОМУ ПИШЕМО ФУНКЦІЮ
-
-// function calculateSalaries(salaries) {
-//   const money = Object.values(salaries);
-//   console.log(`money`, money);
-//   let total = 0;
-
-//   for (let element of money) {
-//     total += element;
-//   }
-//   return total;
-// }
-
-// console.log("result", calculateSalaries(salaries));
-
-
-// 3 ЗАДАЧА) Создайте объект calculator(калькулятор) с тремя методами:
-// read()- запрашивает два значения и сохраняет их как свойства объекта.
-// sum() - возвращает сумму сохранённых значений.
-// mult() - перемножает сохранённые значения и возвращает результат.
-
-// const calculator = {
-//   a: null,//ТУТ МИ ЗАПИСУЄМО, ЩОБ НАШІ ЗНАЧЕННЯ ЗАПИСУВАЛИСЬ У ОБЄКТ
-//   b: null,
-
-//   read() {
-//     this.a = prompt("Enter A value");
-//     this.b = prompt("Enter B value");
-//   },
-
-//   sum() {
-//     return Number(this.a) + Number(this.b);
-//   },
-
-//   mult() {
-//     return this.a * this.b;
-//   },
-// };
-
-// console.log("before read", calculator);
-// calculator.read();
-// console.log("after read", calculator);
-// console.log(`sum`, calculator.sum());
-// console.log(`mult`, calculator.mult());
-
+// checkString("html & css & js tasks")
